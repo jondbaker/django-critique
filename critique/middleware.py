c918678be042b4ddd60f828888851b8f79843cc1
Jonathan D. Baker <jbaker@nexgenstorage.com>
2013-08-10 15:58:14 -0600

    Begin unit tests for CritiqueMiddleware.process_response method.

--- critique/middleware.py
+++ critique/middleware.py
@@ -1,3 +1,4 @@
+import logging
 import re
 
 
@@ -9,8 +10,11 @@ class CritiqueMiddleware(object):
     is_active = None
     mime_types = ["text/plain", "application/xhtml+xml"]
 
+    def _inject(self, request, response):
+        return '<div id="dj-critique"></div>'
+
     def process_request(self, request):
-        """Middleware hook.
+        """Django middleware hook.
 
         :seealso: https://docs.djangoproject/en/1.5/topics/http/middleware/#process-request
 
@@ -26,3 +30,23 @@ class CritiqueMiddleware(object):
                 not re.match("^/admin", request.path)):
             self.is_active = True
         return None
+
+    def process_response(self, request, response):
+        """Django middleware hook.
+
+        :seealso: https://docs.djangoproject/en/1.5/topics/http/middleware/#process-response
+
+        :param request: HTTP request
+        :request type: object
+        :param request: HTTP response
+        :request type: object
+
+        :returns: HTTP response
+        :rtype: object
+        """
+        if self.is_active and response.status_code == 200:
+            try:
+                response.content = self._inject(request, response)
+            except Exception as e:
+                logging.debug("Exception: " + repr(e))
+        return response
--- tests/tests/unit.py
+++ tests/tests/unit.py
@@ -6,36 +6,37 @@ from critique.middleware import CritiqueMiddleware
 
 
 class Request(object):
-    def __init__(self):
-        self.method = "GET"
-        self.META = {"CONTENT_TYPE": "text/plain"}
-        self.path = "/"
+    def __init__(
+            self, method="GET", content_type="text/plain", path="/"):
+        """Happy-path settings"""
+        self.method = method
+        self.META = {"CONTENT_TYPE": content_type}
+        self.path = path
 
 
 class Response(object):
-    pass
+    def __init__(self, status_code=200):
+        """Happy-path settings"""
+        self.status_code = status_code
+        self.content = ""
 
 
 class CritiqueTestCase(TestCase):
     def setUp(self):
         self.critique = CritiqueMiddleware()
 
+    def was_injected(self, response):
+        try:
+            response.lower().index('<div id="dj-critique">')
+        except ValueError:
+            return False
+        else:
+            return True
+
 
 class ProcessRequest(CritiqueTestCase):
-    def test_mime_invalid(self):
-        request = Request()
-        request.META["CONTENT_TYPE"] = "image/jpeg"
-        self.critique.process_request(request)
-        self.assertFalse(self.critique.is_active)
-
-    def test_mime_ok(self):
-        request = Request()
-        self.critique.process_request(request)
-        self.assertTrue(self.critique.is_active)
-
     def test_http_method_invalid(self):
-        request = Request()
-        request.method = "POST"
+        request = Request(method="POST")
         self.critique.process_request(request)
         self.assertFalse(self.critique.is_active)
 
@@ -44,9 +45,18 @@ class ProcessRequest(CritiqueTestCase):
         self.critique.process_request(request)
         self.assertTrue(self.critique.is_active)
 
-    def test_url_invalid(self):
+    def test_mime_invalid(self):
+        request = Request(content_type="image/jpeg")
+        self.critique.process_request(request)
+        self.assertFalse(self.critique.is_active)
+
+    def test_mime_ok(self):
         request = Request()
-        request.path = "/admin"
+        self.critique.process_request(request)
+        self.assertTrue(self.critique.is_active)
+
+    def test_url_invalid(self):
+        request = Request(path="/admin")
         self.critique.process_request(request)
         self.assertFalse(self.critique.is_active)
 
@@ -54,3 +64,36 @@ class ProcessRequest(CritiqueTestCase):
         request = Request()
         self.critique.process_request(request)
         self.assertTrue(self.critique.is_active)
+
+
+class ProcessResponse(CritiqueTestCase):
+#    def test_inject_invalid(self):
+#        # @todo need to pass poorly-formed HTML to raise Exception
+#        self.critique.is_active = True
+#        self.assertRaises(
+#            Exception, self.critique.process_response, Request(), Response())
+
+#    def test_inject_ok(self):
+#        pass
+
+    def test_is_active_invalid(self):
+        request = Request()
+        request.method = "POST"
+        response = self.critique.process_response(request, Response())
+        self.assertFalse(self.was_injected(response.content))
+
+    def test_is_active_ok(self):
+        self.critique.is_active = True
+        response = self.critique.process_response(Request(), Response())
+        self.assertTrue(self.was_injected(response.content))
+
+    def test_response_code_invalid(self):
+        self.critique.is_active = True
+        response = Response(status_code=302)
+        response = self.critique.process_response(Request(), response)
+        self.assertFalse(self.was_injected(response.content))
+
+    def test_response_code_ok(self):
+        self.critique.is_active = True
+        response = self.critique.process_response(Request(), Response())
+        self.assertTrue(self.was_injected(response.content))
