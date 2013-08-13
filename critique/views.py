a473fef517143e52f9c6c552d54479d887951c73
Jonathan D. Baker <jbaker@nexgenstorage.com>
2013-08-13 14:28:39 -0600

    Implement critique view method.

--- critique/views.py
+++ critique/views.py
@@ -1,2 +1,19 @@
+from django.http import Http404, HttpResponse
+
+from .forms import CritiqueForm
+
+
 def create(request):
-    pass
+    """Validates input and then creates a Critique record."""
+    if request.method != "POST" and not request.is_ajax():
+        raise Http404()
+
+    form = CritiqueForm(request.POST)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.url = request.META["HTTP_REFERER"]
+        obj.user_agent = request.META["HTTP_USER_AGENT"]
+        obj.save()
+        return HttpResponse("OK")
+    else:
+        return HttpResponse("ERROR")
